6b75c07ab2e885dfa87dc4e0b9992ff3376ba18b
Lara Isak <luckasti.larry@gmail.com>
2024-01-02 21:14:10 +0100

    💄 Update font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,11 @@
 
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
 // components
 import Navbar from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Navbar />
         {children}
       </body>
