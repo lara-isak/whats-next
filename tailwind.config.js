e5ff527886059d6f6740ee7bd323295d1ed9af32
Lara Isak <luckasti.larry@gmail.com>
2024-01-02 21:14:38 +0100

    💄 Extend Tailwind with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#1d4ed8',
+        secondary: '#f59e0b',
+        dark: '#111827',
+        light: '#f3f4f6',
       },
     },
   },
