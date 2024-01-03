cb120cbca71e733820079db26d676b251b27b1b2
Lara Isak <luckasti.larry@gmail.com>
2024-01-03 20:46:35 +0100

    🎨 Fetch and output list of shows

---
+++ app/shows/ShowsList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+async function getShows() {
+  const res = await fetch("http://localhost:4000/shows");
+
+  return res.json();
+}
+
+export default async function ShowsList() {
+  const shows = await getShows();
+  console.log(shows);
+
+  return (
+    <>
+      {shows.map((show) => (
+        <div key={show.id} className="card my-5">
+          <h3>{show.title}</h3>
+          <p>{show.body.slice(0, 200)}...</p>
+          <div className={`pill ${show.priority}`}>
+            {show.priority} priority
+          </div>
+        </div>
+      ))}
+      {shows.length === 0 && <p className="text-center">No shows to show</p>}
+    </>
+  );
+}
