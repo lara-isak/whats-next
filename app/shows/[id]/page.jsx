a45ee45022619902488377f94b92eaa6b20ff644
Lara Isak <luckasti.larry@gmail.com>
2024-01-06 19:26:18 +0100

    ✨ Create a page for each show and display show data

---
+++ app/shows/[id]/page.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+async function getShow(id) {
+    const res = await fetch("http://localhost:4000/shows/" + id, {
+      next: {
+          revalidate: 60,
+      },
+    });
+  
+    return res.json();
+  }
+
+export default async function ShowDetails({ params }) {
+
+    const show = await getShow(params.id);
+
+    console.log(show);
+
+  return (
+    <main>
+        <nav>
+            <h2>Show Details</h2>
+        </nav>
+        <div className='card'>
+            <h3>{show.title}</h3>
+            <small>Created by {show.user_email}</small>
+            <p>{show.body}</p>
+            <div className={`pill ${show.priority}`}>
+            {show.priority} priority
+          </div>
+        </div>
+    </main>
+  )
+}
