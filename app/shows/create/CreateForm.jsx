8ce6df6fd6d44893438fbb4971de2667753a69cd
Lara Isak <luckasti.larry@gmail.com>
2024-01-08 07:45:57 +0100

    ✨ Create a basic form for adding new shows

--- app/shows/create/CreateForm.jsx
+++ app/shows/create/CreateForm.jsx
@@ -1,8 +1,55 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function CreateForm() {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [priority, setPriority] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div>CreateForm</div>
-  )
+  
+    <form className="w-1/2">
+      <label>
+        <span>Title:</span>
+        <input
+          required 
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+      </label>
+      <label>
+        <span>Body:</span>
+        <textarea
+          required
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+        />
+      </label>
+      <label>
+        <span>Priority:</span>
+        <select 
+          onChange={(e) => setPriority(e.target.value)}
+          value={priority}
+        >
+          <option value="low">Low Priority</option>
+          <option value="medium">Medium Priority</option>
+          <option value="high">High Priority</option>
+        </select>
+      </label>
+      <button 
+        className="btn-primary" 
+        disabled={isLoading}
+      >
+      {isLoading && <span>Adding...</span>}
+      {!isLoading && <span>Add Show</span>}
+    </button>
+    </form>
+    
+  );
 }
