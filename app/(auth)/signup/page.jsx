8defe34209392808ebba30897a4ef651bd86ecff
Lara Isak <luckasti.larry@gmail.com>
2024-02-04 17:26:48 +0100

    ✨ Add error handling and verification page

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,24 +1,36 @@
 "use client";
 import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import AuthForm from "../AuthForm";
 
 export default function Signup() {
+  const router = useRouter();
+  const [error, setError] = useState("");
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
 
     const supabase = createClientComponentClient();
-    await supabase.auth.signUp({
+    const { error } = await supabase.auth.signUp({
       email,
       password,
       options: { emailRedirectTo: `${location.origin}/api/auth/callback` },
     });
+
+    if (error) {
+      setError(error.message);
+    }
+    if (!error) {
+      router.push("/verify");
+    }
   };
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className="error">{error}</div>}
     </main>
   );
 }
